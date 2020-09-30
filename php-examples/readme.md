d3549d71bc1c37152431fbafe3152719d6424c99
Zemian Deng <zemian.deng@oracle.com>
2020-09-30 14:35:16 -0400

    Improve test

---
+++ php-examples/readme.md
@@ -0,0 +1,3 @@
+## PHP with MySQL Setup
+
+Most PHP has MySQL enabled. If not ensure you enable it during compile.
--- php-examples/test.php
+++ php-examples/test.php
@@ -7,12 +7,25 @@ $password = "test123";
 $dbname = "testdb";
 $conn = new mysqli($host, $username, $password, $dbname);
 
-function select($conn) {
+function select_all($conn) {
 	$result = $conn->query('SELECT * FROM test');
+	$ret = [];
 	while($row = $result->fetch_assoc()) {
-	 	var_dump($row);
+		array_push($ret, $row);
 	}
 	$result->close();
+	return $ret;
+}
+
+function select($conn, $id) {
+	$stmt = $conn->prepare('SELECT * FROM test WHERE id = ?');
+	$stmt->bind_param('i', $id);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$ret = $result->fetch_assoc();
+	$result->close();
+	$stmt->close();
+	return $ret;
 }
 
 function select_by_cat($conn, $cat) {
@@ -21,14 +34,13 @@ function select_by_cat($conn, $cat) {
 	$stmt->execute();
 	$result = $stmt->get_result();
 	while($row = $result->fetch_assoc()) {
-	 	var_dump($row);
+		var_dump($row);
 	}
 	$result->close();
 	$stmt->close();
 }
 
 function select_total($conn, $cat) {
-	$ret = 0;
 	$stmt = $conn->prepare('SELECT sum(price) AS total FROM test WHERE cat = ?');
 	$stmt->bind_param('s', $cat);
 	$stmt->execute();
@@ -36,49 +48,62 @@ function select_total($conn, $cat) {
 	$ret = $result->fetch_row()[0];
 	$result->close();
 	$stmt->close();
-
 	return $ret;
 }
 
 function insert($conn, $cat, $price, $qty) {
+	$ret = null;
 	$stmt = $conn->prepare('INSERT INTO test(cat, price, qty) VALUES (?, ?, ?)');
 	$stmt->bind_param('sdi', $cat, $price, $qty);
 	$stmt->execute();
 	if($stmt->affected_rows) {
-	 	echo "Insert id=" . $stmt->insert_id . "\n";
+		$ret = $stmt->insert_id;
 	}
 	$stmt->close();
+	return $ret;
 }
 
 function update($conn, $id, $price, $qty) {
+	$ret = null;
 	$stmt = $conn->prepare('UPDATE test SET price = ?, qty = ? WHERE id = ?');
 	$stmt->bind_param('dii', $price, $qty, $id);
 	$stmt->execute();
 	if($stmt->affected_rows) {
-	 	echo "Updated id=" . $id . "\n";
+		$ret = $id;
 	}
 	$stmt->close();
+	return $ret;
 }
 
 function delete($conn, $id) {
+	$ret = null;
 	$stmt = $conn->prepare('DELETE FROM test WHERE id = ?');
 	$stmt->bind_param('i', $id);
 	$stmt->execute();
 	if($stmt->affected_rows) {
-	 	echo "Deleted id=" . $id . "\n";
+		$ret = $id;
 	}
 	$stmt->close();
+	return $ret;
 }
 
 try {
-    //select($conn);
-    //select_by_cat($conn, 'test');
-    //insert($conn, 'php', 0.10, 1);
-    //insert($conn, 'php', 0.20, 2);
-    //echo "total: " . select_total($conn, 'php') . "\n";
-	//update($conn, 13, 0.99, 10);
-	//delete($conn, 13);
-    select_by_cat($conn, 'php');
+	// var_dump(select_all($conn));
+	// var_dump(select($conn, 1));
+	// var_dump(select_by_cat($conn, 'test'));
+
+	// insert($conn, 'php', 0.10, 1);
+	// insert($conn, 'php', 0.20, 2);
+	// select_by_cat($conn, 'php');
+	
+	// var_dump(select_total($conn, 'php'));
+	// update($conn, 26, 0.99, 10);
+	// var_dump(select_total($conn, 'php'));
+	
+	// var_dump(select($conn, 26));
+	// var_dump(delete($conn, 26));
+	
+	select_by_cat($conn, 'php');
 } finally {
-    $conn->close();
+	$conn->close();
 }
