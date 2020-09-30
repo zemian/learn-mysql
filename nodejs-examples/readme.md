a00215e35331c28d2463efc9c8dcc5a6f3dfc586
Zemian Deng <zemian.deng@oracle.com>
2020-09-30 09:04:13 -0400

    Add nodejs-examples

---
+++ nodejs-examples/readme.md
@@ -0,0 +1,9 @@
+## Using `mysql` NodeJS driver 
+
+To install:
+    
+    npm install mysql
+
+The `mysql` nodejs driver automatically abstract the query parameters that you don't need to create prepared statement explicitly!
+
+See https://github.com/mysqljs/mysql#readme
---
+++ nodejs-examples/src/test.js
@@ -0,0 +1,55 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'zemian',
+  password : 'test123',
+  database : 'zemiandb'
+});
+connection.connect();
+
+function select(connection) {
+	connection.query('SELECT * FROM test', function (error, results, fields) {
+	  if (error) throw error;
+	  results.forEach(row => console.log(row));
+	});	
+}
+
+function selectByCat(connection, cat) {
+	connection.query('SELECT * FROM test WHERE cat = ?', [cat], function (error, results, fields) {
+	  if (error) throw error;
+	  results.forEach(row => console.log(row));
+	});
+}
+
+function insert(connection, cat, price, qty) {
+	connection.query('insert into test(cat, price, qty) values (?, ?, ?)', [cat, price, qty], function (error, results, fields) {
+	  if (error) throw error;
+	  console.log("Insert result=", results);
+	});
+}
+
+function update(connection, id, qty) {
+	connection.query('update test set qty = ? where id=?', [qty, id], function (error, results, fields) {
+	  if (error) throw error;
+	  console.log("Update result=", results);
+	});
+}
+
+function deleteById(connection, id) {
+	connection.query('delete from test where id = ?', [id], function (error, results, fields) {
+	  if (error) throw error;
+	  console.log("Delete result=", results);
+	});
+}
+
+try {
+	// Run example
+	//select(connection);
+	//insert(connection, 'nodejs', 90, 1);
+	//insert(connection, 'nodejs', 90, 2);
+	// update(connection, 13, 10);
+	selectByCat(connection, 'nodejs');
+	//deleteById(connection, 13);
+} finally {
+	connection.end();	
+}
