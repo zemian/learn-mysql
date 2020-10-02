de8d2956b051950c205e79cb000db159e081b47d
Zemian Deng <zemian.deng@oracle.com>
2020-10-02 09:37:48 -0400

    Add .end() notes

--- nodejs-examples/src/hello.js
+++ nodejs-examples/src/hello.js
@@ -1,4 +1,5 @@
 // A simple DB connection test 
+// https://github.com/mysqljs/mysql#readme
 var mysql = require('mysql');
 var conn = mysql.createConnection({
   host     : 'localhost',
@@ -14,6 +15,9 @@ try {
 	  console.log('Connection successful! Test Result: ', results[0].result);
 	});
 } finally {
+	// This only works because we are not using the connection pool, and 
+	// it actually will wait for conn queries to complete before it ends it!
+	// TODO: A better way is to wrap it in Promise object.
 	conn.end();
 }
  
