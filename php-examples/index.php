266cd9bb388d2972121162e0b62f10b0d848483c
Zemian Deng <zemian.deng@oracle.com>
2021-03-02 13:48:56 -0500

    Add index.php

---
+++ php-examples/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learn MySQL</title>
+    <link rel="stylesheet" href="https://unpkg.com/bulma">
+</head>
+<body>
+
+<div class="hero is-primary">
+    <div class="hero-body">
+        <h1 class="title">Learn MySQL Database</h1>
+    </div>
+</div>
+<div class="section">
+    <div class="columns is-centered">
+        <div class="column is-3">
+            <div class="notification is-info has-text-centered">
+                <a href="hello.php">Hello</a>
+            </div>
+        </div>
+        <div class="column is-3">
+            <div class="notification is-info has-text-centered">
+                <a href="hello-pdo.php">Hello PDO</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
