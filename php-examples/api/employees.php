5188098267c4174bfb1e2571239bbe45ad93c73c
Zemian Deng <zemiandeng@gmail.com>
2021-03-02 16:57:54 -0500

    Reformat code

--- php-examples/api/employees.php
+++ php-examples/api/employees.php
@@ -1,10 +1,9 @@
 <?php
+header('Content-Type: application/json');
+
 $config = json_decode(file_get_contents("../env.json"));
 $db = $config->employeesdb;
 $conn = new PDO($db->dns, $db->username, $db->passwd);
-
-header('Content-Type: application/json');
-
 $stmt = $conn->query('SELECT * FROM employees ORDER BY RAND() LIMIT 25');
 $employees = $stmt->fetchAll(PDO::FETCH_CLASS);
 echo json_encode(array(
